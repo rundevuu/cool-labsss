73936bb91e57d976816f7e4e9951565c25758c9f
rundevuu <93253448+rundevuu@users.noreply.github.com>
2021-11-16 17:25:52 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,121 +1,87 @@
-#include <cstddef>
-#include <locale.h>
 #include <stdio.h>
-
-//********* Стек ***********************************
-#define MAX_STACK_SIZE 100
-#define I_AM_NO_A_VALID_RESULT 9999
-typedef int StackElement;
-
-// TODO реализуй меня
-struct Stack {
-	char a[MAX_STACK_SIZE];
-	size_t size;
-	int top
-};
-
-// Проинициализировать внутреннее содержимое структуры стека
-void stack_init(struct Stack* s) {
-	s->top = 0;
-}
-
-// Вернуть количество элементов в стеке
-size_t stack_size(struct Stack* s) {
-	return s->top;
-}
-
-// Вернуть значение на верхушке стека, без изменения верхушки
-StackElement stack_peek(struct Stack* s) {
-	if ((s->top) > 0) {
-		return s->a[s->top - 1];
+#include <stdlib.h>
+#include <math.h>
+int HashFunc(unsigned char* c, int* N, int* size) {
+	int inc = 0;
+	for (int i = 0; i < *N; ++i) {
+		inc += c[i] % 3 * *size;
+		*size *= 3;
 	}
-
+	*size /= 3;
+	return inc;
 }
 
-// TODO реализуй меня
-// Поместить значение x на верхушку стека
-void stack_push(struct Stack* s, StackElement x) {
-	if (s->top < MAX_STACK_SIZE) {
-		s->a[s->top] = x;
-		s->top++;
-	}
-	else
-		printf("Стек полон, количество элементов: %d !\n", s->top);
-}
+int ReadTemplate(FILE* input, unsigned char* template, int* length) {
+	for (int i = 0; i < 17; ++i) {
+		if (!(template[i] = fgetc(input)))
+			return 0;
+		if (template[i] == '\n') {
+			template[i] = '\0';
+			break;
+		}
+		++* length;
 
-// Достать значение с верхушки стека
-StackElement stack_pop(struct Stack* s) {
-	if ((s->top) > 0) {
-		return s->a[s->top - 1];
 	}
-	else {
-		printf("Стек пуст!\n");
+	return 1;
+}
+int ReadPart(FILE* input, unsigned char* part, int* length) {
+	if (!fread(part, 1, *length, input)) {
 		return 0;
 	}
+	if (feof(input))
+		return 0;
+	return 1;
 }
-#define check_expr(expr, expected) {\
-	int result = (expr); \
-	if (result != (expected)) {\
-		printf("Ожидал:\t '" #expr "'\t to == '%d', получил '%d' \t(функция %s в строке %d).\n", \
-			   (expected), result, __FUNCTION__, __LINE__);\
-		error_count++;\
-	}\
+int NextHash(FILE* input, int* text_hash, unsigned char* part, int* length, int* size) {
+	*text_hash = *text_hash / 3;
+	for (int i = 1; i < *length; ++i)
+		part[i - 1] = part[i];
+	if (!(part[*length - 1] = fgetc(input)))
+		return 0;
+	*text_hash += part[*length - 1] % 3 * *size;
+	if (feof(input))
+		return 0;
+	return 1;
 }
-
-int error_count = 0;
-
-void test_stack() {
-	printf("\nТест 1\n");
-	{
-		struct Stack empty_stack;
-		stack_init(&empty_stack);
-		check_expr(stack_size(&empty_stack), 0);
-	}
-	printf("\nТест 2\n");
-	{
-		struct Stack simple_stack;
-		stack_init(&simple_stack);
-
-		stack_push(&simple_stack, 5);
-		check_expr(stack_peek(&simple_stack), 5);
-		check_expr(stack_size(&simple_stack), 1);
-	}
-	printf("\nТест 3\n");
-	{
-		struct Stack push_pop_stack;
-		stack_init(&push_pop_stack);
-
-		stack_push(&push_pop_stack, 5);
-		check_expr(stack_pop(&push_pop_stack), 5);
-	}
-	printf("\nТест 4\n");
-	{
-		struct Stack complex_stack;
-		stack_init(&complex_stack);
-		for (int i = 1; i <= 80; i++) {
-			stack_push(&complex_stack, i);
+void IsIt(unsigned char* template, unsigned char* part, int* length, int* count) {
+	for (int i = 0; i < *length; ++i)
+		if (template[i] == part[i])
+			printf("%d ", *count + i + 1);
+		else {
+			printf("%d ", *count + i + 1);
+			return;
 		}
-		for (int i = 80; i > 40; i--) {
-			stack_pop(&complex_stack);
-
+}
+void RobinCarpSearch(FILE* input, unsigned char* template, int* length) {
+	int text_hash, template_hash, count = 0, size = 1, s = 1;
+	unsigned char part[17];
+	template_hash = HashFunc(template, length, &size);
+	printf("\n%d ", template_hash);
+	if (!ReadPart(input, part, length))
+		return;
+	text_hash = HashFunc(part, length, &s);
+	while (1) {
+		if (text_hash == template_hash)
+			IsIt(template, part, length, &count);
+		if (!NextHash(input, &text_hash, part, length, &size)) {
+			return;
 		}
-		check_expr(stack_size(&complex_stack), 40);
-		check_expr(stack_peek(&complex_stack), 40);
-		check_expr(stack_pop(&complex_stack), 40);
+		++count;
 	}
 }
 
-//****************************************************************
 int main() {
-	setlocale(LC_ALL, "Russian");
-	test_stack();
-
-	if (error_count) {
-		printf("\n\n!!!!!!!!!! Обнаружены ошибки (см. выше), исправьте и попробуйте снова !!!!!!!!!!!!!!!\n");
-		return 1;
-	}
-	else {
-		printf("\n\n************ Все тесты прошли *************\n");
+	FILE* input;
+	input = fopen("in.txt", "rt");
+	if (input == NULL)
+		return 0;
+	unsigned char template[17];
+	int length = 0;
+	if (!ReadTemplate(input, template, &length)) {
+		fclose(input);
 		return 0;
 	}
+	RobinCarpSearch(input, template, &length);
+	fclose(input);
+	return 0;
 }
